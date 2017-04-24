edf98174ef92e7bb48cfbc021e876bc120166b39
Sebastien Ronsse <sronsse@gmail.com>
2017-04-23 19:27:57 -0700

    Added basic HUD class (generated from editor)

---
+++ Source/ImGuiHUD/MyImGuiHUD.cpp
@@ -0,0 +1,8 @@
+// Copyright 2017 Sebastien Ronsse
+
+#include "ImGuiHUD.h"
+#include "MyImGuiHUD.h"
+
+
+
+
---
+++ Source/ImGuiHUD/MyImGuiHUD.h
@@ -0,0 +1,19 @@
+// Copyright 2017 Sebastien Ronsse
+
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "MyImGuiHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IMGUIHUD_API AMyImGuiHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
