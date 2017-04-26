1d5363d6cbac083be8d280986bcc2bca803c6db9
Sebastien Ronsse <sronsse@gmail.com>
2017-04-26 15:30:19 -0700

    Added basic ImGui handling skeleton within custom HUD class

--- Source/ImGuiHUD/MyImGuiHUD.cpp
+++ Source/ImGuiHUD/MyImGuiHUD.cpp
@@ -3,6 +3,56 @@
 #include "ImGuiHUD.h"
 #include "MyImGuiHUD.h"
 
+void AMyImGuiHUD::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
 
+	// Setup ImGui binding
+	ImGui_ImplUE_Init();
+}
 
+void AMyImGuiHUD::BeginDestroy()
+{
+	Super::BeginDestroy();
 
+	// Cleanup
+	ImGui_ImplUE_Shutdown();
+}
+
+void AMyImGuiHUD::DrawHUD()
+{
+	// Process events
+	ImGui_ImplUE_ProcessEvent();
+
+	// Prepare new frame
+	ImGui_ImplUE_NewFrame();
+
+	// Rendering
+	ImGui::Render();
+}
+
+bool AMyImGuiHUD::ImGui_ImplUE_Init()
+{
+	return true;
+}
+
+void AMyImGuiHUD::ImGui_ImplUE_Shutdown()
+{
+}
+
+bool AMyImGuiHUD::ImGui_ImplUE_CreateDeviceObjects()
+{
+	return true;
+}
+
+void AMyImGuiHUD::ImGui_ImplUE_InvalidateDeviceObjects()
+{
+}
+
+void AMyImGuiHUD::ImGui_ImplUE_ProcessEvent()
+{
+}
+
+void AMyImGuiHUD::ImGui_ImplUE_NewFrame()
+{
+}
--- Source/ImGuiHUD/MyImGuiHUD.h
+++ Source/ImGuiHUD/MyImGuiHUD.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "GameFramework/HUD.h"
+#include "imgui/imgui.h"
 #include "MyImGuiHUD.generated.h"
 
 /**
@@ -13,7 +14,14 @@ class IMGUIHUD_API AMyImGuiHUD : public AHUD
 {
 	GENERATED_BODY()
 	
-	
-	
-	
+	virtual void PostInitializeComponents() override;
+	virtual void BeginDestroy() override;
+	virtual void DrawHUD() override;
+
+	bool ImGui_ImplUE_Init();
+	void ImGui_ImplUE_Shutdown();
+	bool ImGui_ImplUE_CreateDeviceObjects();
+	void ImGui_ImplUE_InvalidateDeviceObjects();
+	void ImGui_ImplUE_ProcessEvent();
+	void ImGui_ImplUE_NewFrame();
 };
